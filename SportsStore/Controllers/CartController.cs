16d6d7fb40841c732b9fb986503dc6faf36cb667
Margarita Smoliakova <msmoliakova@servicechannel.com>
2017-09-26 18:41:07 +0300

    state before Administration chapter 11

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -10,15 +10,17 @@ namespace SportsStore.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private Cart cart;
 
-        public CartController(IProductRepository repo)
+        public CartController(IProductRepository repo, Cart cartService)
         {
             repository = repo;
+            cart = cartService;
         }
 
         public ViewResult Index(string returnUrl) => View(new CartIndexViewModel
         {
-            Cart = GetCart(),
+            Cart = cart,
             ReturnUrl = returnUrl
         });
 
@@ -29,9 +31,7 @@ namespace SportsStore.Controllers
 
             if (product != null)
             {
-                Cart cart = GetCart();
-                cart.AddItem(product, 1);
-                SaveCart(cart);
+                cart.AddItem(product, 1);                
             }
 
             return RedirectToAction("Index", new { returnUrl });
@@ -43,21 +43,12 @@ namespace SportsStore.Controllers
 
             if (product != null)
             {
-                Cart cart = GetCart();
-                cart.RemoveLine(product);
-                SaveCart(cart);
+                cart.RemoveLine(product);                
             }
 
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        private Cart GetCart()
-        {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-
-            return cart;
-        }
-
-        private void SaveCart(Cart cart) => HttpContext.Session.SetJson("Cart", cart);
+        
     }
 }
